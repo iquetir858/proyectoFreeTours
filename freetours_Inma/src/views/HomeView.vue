582ce1f67d12602ae3aad1566c8ef31d75ba0a15
Inma Quesada Tirado <iquetir858@g.educaand.es>
2025-02-24 13:53:02 +0100

    Filtros busqueda home

--- freetours_Inma/src/views/HomeView.vue
+++ freetours_Inma/src/views/HomeView.vue
@@ -2,15 +2,10 @@
 import Card from '@/components/Card.vue';
 import { ref } from 'vue';
 
-/*
-let rutas = ref([
-  { imagen: 'https://s2.wklcdn.com/image_267/8023824/165381351/104033852.400x300.jpg', titulo: 'Ruta Histórica', localidad: 'Jaén', fecha: '2025-02-10' },
-  { imagen: 'https://www.losmundosdeceli.com/wp-content/uploads/2018/12/ruta-granada-acera-darro.jpg', titulo: 'Ruta Cultural', localidad: 'Granada', fecha: '2025-02-12' },
-  { imagen: 'https://www.guiategalicia.com/wp-content/uploads/2017/06/fullsizerender-2.jpg', titulo: 'Ruta Nocturna', localidad: 'Galicia', fecha: '2025-02-14' },
-  { imagen: 'https://www.comunidad.madrid/sites/default/files/styles/imagen_enlace_opcional/public/iglesia_parroquial_de_san_salvador_en_leganes_01.jpg?itok=w7jAtBKN', titulo: 'Ruta Iglesia Salvador', localidad: 'Leganés', fecha: '2025-02-26' }
-]);*/
+let rutasDisponibles = ref([]);
+let localidad = ref();
+let fecha = ref();
 
-let rutasDisponibles = ref();
 
 //Función que hace la petición a la base de datos para obtener
 //las rutas generales
@@ -29,6 +24,38 @@ function obtenerRutasDisponibles() {
 }
 obtenerRutasDisponibles();
 
+
+/**
+ * Función que realiza la búsqueda de una ruta según una fecha y/o localidad
+ */
+function busquedaFiltro() {
+
+  if (fecha) {
+    fetch(`/api/api.php/rutas?fecha=${fecha.value}&localidad=${localidad.value}`, {
+      method: 'GET',
+    })
+      .then(response => response.json())
+      .then(data => {
+        console.log(fecha.value);
+
+        console.log('Rutas filtradas:', data);
+        rutasDisponibles.value = data;
+      })
+      .catch(error => console.error('Error en búsqueda de ruta con filtro:', error));
+
+  }
+}
+
+/**
+ * Función que elimina los filtros aplicados, muestra toda las rutas disponibles de nuevo
+ * y elimina los valores de los inputs de localidad y fecha
+ */
+function borrarFiltros() {
+  obtenerRutasDisponibles();
+  fecha.value = '';
+  localidad.value = '';
+}
+
 </script>
 
 <template>
@@ -42,18 +69,22 @@ obtenerRutasDisponibles();
       <form action="" class="d-flex flex-row align-items-center justify-content-evenly">
         <label for="localidadBusqueda" class="me-2">Localidad:</label>
         <!--Cambiar esto a un select?-->
-        <input type="text" name="localidadBusqueda" placeholder="Introduce una localidad" class="me-5">
+        <input v-model="localidad" type="text" name="localidadBusqueda" placeholder="Introduce una localidad"
+          class="me-5">
 
         <label for="fechaBusqueda" class="me-2">Fecha:</label>
-        <input type="date" name="fechaBusqueda">
+        <input v-model="fecha" type="date" name="fechaBusqueda">
 
-        <button class="btn ms-2 btnBusqueda" aria-label="Realizar búsqueda de rutas">
+        <button @click.prevent="busquedaFiltro" class="btn ms-2 btnBusqueda" aria-label="Realizar búsqueda de rutas">
           <i class="fa-solid fa-magnifying-glass"></i>
         </button>
+        <button @click.prevent="borrarFiltros" class="btn btnBusqueda ms-2"
+          aria-label="Eliminar filtros aplicados">X</button>
 
       </form>
     </div>
-    <Card v-if="rutasDisponibles" :propRutas="rutasDisponibles"></Card>
+    <Card v-if="rutasDisponibles.length > 0" :propRutas="rutasDisponibles"></Card>
+    <div v-else class="container bg-secondary text-white mt-2">No existen rutas para dicha fecha</div>
 
   </main>
 </template>
