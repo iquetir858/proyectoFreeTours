bb7f0c9fb177797acddee4c59eb164c633dc228f
Inma Quesada Tirado <iquetir858@g.educaand.es>
2025-02-25 23:02:50 +0100

    Selección guía pt1 (no funciona)

--- freetours_Inma/src/views/AdminListRoutes.vue
+++ freetours_Inma/src/views/AdminListRoutes.vue
@@ -1,8 +1,20 @@
 <script setup>
-import { ref } from 'vue';
+import { ref, onMounted } from 'vue';
 
 let rutasBD = ref();
+let guiasDisponiblesPorFecha = ref({});
+//----------------MODAL
+let exitoActualizacion = ref('');
+let errorActualizacion = ref('');
+let modalConfirmacion = null;
+onMounted(() => {
+    modalConfirmacion = new bootstrap.Modal(document.getElementById('modalConfirmacion'));
+});
+function cerrarModal() {
+    modalConfirmacion.hide();
+}
 
+//----------------FUNCIONES PARA OBTENER RUTAS/GUÍAS
 function obtenerRutas() {
     //Obtenemos todas las rutas
     //fetch('http://localhost/api/api.php/rutas', {
@@ -11,21 +23,105 @@ function obtenerRutas() {
     })
         .then(response => response.json())
         .then(data => {
-            //console.log('Rutas:', data);
-            console.log(data);
+            console.log('Rutas:', data);
             rutasBD.value = data;
+            cargarGuiasPorFecha();
         })
-        .catch(error => console.error('Error Listado Rutas:', error));
-
-}
-obtenerRutas();
-
-function borrarRuta(id){
+        .catch(error => console.error('Error (Listado Rutas):', error));
 
 }
 
+/**
+ * Función que, dada la fecha de la ruta, almacena en guiasDisponiblesPorFecha
+ * todos los guias disponibles en dicha fecha
+ * @param fechaRuta 
+ */
+function obtenerGuiasFecha(fechaRuta) {
+    try {
+        //fetch('http://localhost/api/api.php/usuarios', {
+        fetch(`/api/api.php/asignaciones?fecha=${fechaRuta}`, {
+            method: 'GET'
+        })
+            .then(response => response.json())
+            .then(data => {
+                guiasDisponiblesPorFecha.value[fechaRuta] = data;
+                console.log("Guías disponibles en esa fecha: " + JSON.stringify(guiasDisponiblesPorFecha.value));
+                //error.value = '';
+            })
+            .catch(errMsg => {
+                //error.value = errMsg;
+                console.log("Error (obtención guias disponibles): " + errMsg);
+                guiasDisponiblesPorFecha.value[fechaRuta] = []; //Para evitar errores
+            }
+            );
+    } catch (err) {
+        //error.value = 'Error al cargar los datos';
+        console.log("Error Obtención Guías: " + err);
+        guiasDisponiblesPorFecha.value[fechaRuta] = [];
 
 
+    }
+}
+
+/**
+ * Función que, una vez se han cargado las rutas disponibles, llama a la función de obtener 
+ * los guias disponibles por fecha para acceder a ellos en el select
+ */
+function cargarGuiasPorFecha() {
+    rutasBD.value.forEach(ruta => {
+        obtenerGuiasFecha(ruta.fecha);
+    });
+}
+
+/**
+ * Función que se ejecuta cada vez que se cambia el guía en el select e indica que
+ * se ha realizado el cambio o ha ocurrido un error
+ * @param ruta 
+ */
+function cambiarGuia(ruta) {
+    //Como con el v-model se cambia el valor del id del guia directamente,
+    //aquí se realiza un guardado /update en la api
+    const asignacionData = {
+        ruta_id: ruta.id, //ID de la ruta
+        guia_id: ruta.guia_id  //ID del guía
+    };
+
+
+    fetch('/api/api.php/asignaciones', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(asignacionData)
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Respuesta:', data);
+            if (data.status == 'success') {
+                exitoActualizacion.value = data.message;
+                errorActualizacion.value = '';
+            } else {
+                exitoActualizacion.value = '';
+                errorActualizacion.value = data.message;
+            }
+
+            setTimeout(() => {
+                modalConfirmacion.hide();
+                errorActualizacion.value = '';
+                exitoActualizacion.value = '';
+            })
+        })
+        .catch(error => console.error('Error:', error));
+
+    //Luego vuelve a cargar los guias por fecha para que se actualice el valor por defecto del select
+   // cargarGuiasPorFecha();
+}
+
+function borrarRuta(id) {
+}
+
+//------------LLAMADAS AL CARGAR LA VISTA
+obtenerRutas(); 
 </script>
 
 <template>
@@ -51,11 +147,13 @@ function borrarRuta(id){
                     <td>{{ ruta.titulo }}</td>
                     <td>{{ ruta.asistentes }}</td>
                     <td>
-                        {{ ruta.guia_nombre }} +
                         <!--Meter aquí un el guía según la ruta-->
                         <!--ESTO HAY QUE IMPLEMENTARLO-->
-                        <select v-model="ruta.guia_nombre" @focus="alert('hola')" @change="cambiarGuia(ruta.id)">
-                            <option value="admin">Admin</option><!--Aquídeberían ser los guias disponibles en esa fecha-->
+                        <select v-model="ruta.guia_id" @change="cambiarGuia(ruta)">
+                            <option :value="ruta.guia_id">{{ ruta.guia_nombre }}</option>
+                            <option v-for="guia in (guiasDisponiblesPorFecha[ruta.fecha] || [])" :key="guia.id"
+                                :value="guia.id"> {{ guia.nombre }}</option>
+                            <!--Aquí deberían ser los guias disponibles en esa fecha-->
                         </select>
                     </td>
                     <td>
@@ -63,11 +161,32 @@ function borrarRuta(id){
                     </td>
                     <td>
                         <!--Mostrar modal de confirmación de la cancelación-->
-                        <button @click.prevent="borrarRuta(ruta.id)" class="btn btnBorrado" aria-label="Cancelar la ruta">Cancelar</button>
+                        <button @click.prevent="borrarRuta(ruta.id)" class="btn btnBorrado"
+                            aria-label="Cancelar la ruta">Cancelar</button>
                     </td>
                 </tr>
             </tbody>
         </table>
+    </div>
+
+    <!--MODAL DE CONFIRMACIÓN DE ACTUALIZACIÓN-->
+    <div class="text-success bg-color-success text-black">
+        <div class="modal fade" id="modalConfirmacion" tabindex="-1" aria-labelledby="infoModalLabel"
+            aria-hidden="true">
+            <div class="modal-dialog modal-dialog-centered ">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="infoModalLabel">Actualización de guía</h5>
+                        <button type="button" @click.prevent="cerrarModal" class="btn-close" data-bs-dismiss="modal"
+                            aria-label="Cerrar"></button>
+                    </div>
+                    <div class="modal-body">
+                        <p v-if="exitoActualizacion != ''" class="text-success">{{ exitoActualizacion }}</p>
+                        <p v-else class="text-danger">{{ errorActualizacion }}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 </template>
 
