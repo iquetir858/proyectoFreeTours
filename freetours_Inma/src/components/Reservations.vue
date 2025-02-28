0c358543333585203bcb11c905e439d5d350e403
Inma Quesada Tirado <iquetir858@g.educaand.es>
2025-02-28 22:58:43 +0100

    Cancelación de reserva (cliente pt2)

--- freetours_Inma/src/components/Reservations.vue
+++ freetours_Inma/src/components/Reservations.vue
@@ -7,25 +7,71 @@ const props = defineProps({
     valoracion: Boolean //Valor para 
 });
 
+//EMITs 
+const emit = defineEmits(["actualizarReservas"]);
+
 //Variables:
-let rutaSeleccionada = ref(null);
+let reservaSeleccionada = ref(null);
 let modalPersonas = null;
-let modalCancelación = null;
+let modalCancelacion = null;
 let modificacionPersonas = ref(''); //Mensaje al modificar el num de personas que asisten
-let cancelacion = ref(''); //Mensaje al cancelar reserva(?)
+let errorCancelacion = ref(''); //Mensaje de error al cancelar reserva
+let exitoCancelacion = ref(''); //Mensaje de éxito al cancelar reserva
 
 onMounted(() => {
     modalPersonas = new bootstrap.Modal(document.getElementById('modalPersonas'));
-    modalCancelación = new bootstrap.Modal(document.getElementById('modalCancelación'));
+    modalCancelacion = new bootstrap.Modal(document.getElementById('modalCancelacionReserva'));
 });
 
 /**
- * Función que cierra / oculta el modal de info de la ruta
+ * Función que oculta/cierra los modales
  */
 function cerrarModal() {
     modalPersonas.hide();
-    modalCancelación.hide();
-    rutaSeleccionada.value = null;
+    modalCancelacion.hide();
+    reservaSeleccionada.value = null;
+}
+
+/**
+ * Función que muestra el modal para confirmar o cancelar la reserva 
+ * @param reserva Reserva que será almacenada como selección 
+ */
+function mostrarModalCancelacion(reserva) {
+    reservaSeleccionada.value = reserva;
+    modalCancelacion.show();
+}
+
+/**
+ * Función que cancela la reserva seleccionada 
+ */
+function cancelarReserva() {
+    let reservaId = reservaSeleccionada.value.reserva_id;
+    console.log("ReservaId: " + reservaId);
+
+    fetch(`/api/api.php/reservas?id=${reservaId}`, {
+        method: 'DELETE',
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Respuesta:', data);
+            if (data.status == 'success') {
+                exitoCancelacion.value = data.message;
+                errorCancelacion.value = '';
+                //'Emitimos' evento para que se actualicen las tablas con las rutas
+                emit('actualizarReservas');
+            } else {
+                errorCancelacion.value = data.message;
+                exitoCancelacion.value = ''
+            }
+
+            setTimeout(() => {
+                errorCancelacion.value = '';
+                exitoCancelacion.value = '';
+                reservaSeleccionada.value = null; //Eliminamos la ruta seleccionada 
+                modalCancelacion.hide();
+            }, 3000);
+        })
+        .catch(error => console.error('Error:', error));
 }
 
 </script>
@@ -59,10 +105,36 @@ function cerrarModal() {
                                             class="fa-solid fa-pen-to-square"></i>
                                     </button>
                                 </div>
-                                <button class="btn btn-danger w-100 mt-3">Cancelar</button>
+                                <button @click.prevent="mostrarModalCancelacion(reserva)"
+                                    class="btn btn-danger w-100 mt-3">Cancelar Reserva</button>
                             </div>
                         </div>
                     </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <!--Modal de cancelación de reserva-->
+    <div class="modal fade" id="modalCancelacionReserva" tabindex="-1" aria-labelledby="exampleModalLabel">
+        <div class="modal-dialog">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Cancelar reserva</h5>
+                    <button type="button" @click.prevent="cerrarModal" class="btn-close" data-bs-dismiss="modal"
+                        aria-label="Cerrar modal"></button>
+                </div>
+                <div class="modal-body">
+                    <p>La reserva de la ruta se cancelará. ¿Estás seguro?</p>
+                    <p v-if="exitoCancelacion.value != ''" class="text-success">{{ exitoCancelacion }}</p>
+                    <p v-else-if="errorCancelacion.value != ''" class="text-danger">{{ errorCancelacion }}</p>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" @click.prevent="cerrarModal" class="btn"
+                        data-bs-dismiss="modal">Cerrar</button>
+                    <button type="button" @click.prevent="cancelarReserva" class="btn btnBorrado">
+                        Cancelar Reserva
+                    </button>
                 </div>
             </div>
         </div>
--- freetours_Inma/src/views/Client.vue
+++ freetours_Inma/src/views/Client.vue
@@ -19,7 +19,11 @@ function obtenerReservas(emailUsuario) {
         .then(response => response.json())
         .then(data => {
             console.log('Reservas del usuario:', data);
-            let fechaActual = new Date();
+            let fechaActual = new Date(); 
+            //Primero ordenamos las reservas de data según la fecha más próxima a la actual
+            data.sort((a, b) => new Date(a.ruta_fecha) - new Date(b.ruta_fecha));
+            //Ahora filtramos las reservas según si ya han ocurrido, para poder diferenciar
+            //entre aquellas que podemos modificar el num de personas y las que se pueden valorar
             reservasActuales.value = data.filter(elem => new Date(elem.ruta_fecha) >= fechaActual);
             reservasPasadas.value = data.filter(elem => new Date(elem.ruta_fecha) < fechaActual);
         })
@@ -27,7 +31,17 @@ function obtenerReservas(emailUsuario) {
 
 }
 
+/**
+ * Función llamada desde el emit del componente REservations (tarjetas con las reservas actuales)
+ * para actualizarel listado de reservas general (ocurre después del borrado)
+ */
+function actualizarReservasActuales(){
+    obtenerReservas(cliente.value.email);
+}
+
+//Llamada principal al abrir la página del cliente
 obtenerReservas(cliente.value.email);
+
 </script>
 
 <template>
@@ -35,11 +49,11 @@ obtenerReservas(cliente.value.email);
     <!--Mismo componente que en la pág principal?-->
     <!-- Contenedor principal para las reservas -->
     <h2 class="text-center mt-2">Rutas reservadas</h2>
-   <Reservations :reservas="reservasActuales" :valoracion="true"></Reservations>
+   <Reservations v-if="reservasActuales" @actualizar-reservas="actualizarReservasActuales" :reservas="reservasActuales" :valoracion="true"></Reservations>
 
 
    <h2 class="text-center mt-2">Rutas pasadas</h2>
-   <Reservations :reservas="reservasPasadas" :valoracion="true"></Reservations>
+   <Reservations  v-if="reservasPasadas" :reservas="reservasPasadas" :valoracion="true"></Reservations>
 
     <!--Tabla rutas realizadas para valorar-->
 
